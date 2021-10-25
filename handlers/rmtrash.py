cf6a326cb7a8d52654c35796cfec1f40bec0588d
levina <82658782+levina-lab@users.noreply.github.com>
2021-10-25 19:50:30 +0700

    `reverted`
    uploaded the old source

---
+++ handlers/rmtrash.py
@@ -0,0 +1,26 @@
+# function to remove the downloaded files
+
+import os
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from config import BOT_USERNAME
+from helpers.decorators import errors, sudo_users_only
+from helpers.filters import command
+
+downloads = os.path.realpath("downloads")
+raw = os.path.realpath("raw_files")
+
+
+@Client.on_message(command(["rmd", "clean", f"rmd@{BOT_USERNAME}", f"clean@{BOT_USERNAME}"]) & ~filters.edited)
+@errors
+@sudo_users_only
+async def clear_downloads(_, message: Message):
+    ls_dir = os.listdir(downloads)
+    if ls_dir:
+        for file in os.listdir(downloads):
+            os.remove(os.path.join(downloads, file))
+        await message.reply_text("✅ **removed all downloaded files**")
+    else:
+        await message.reply_text("❌ **no files is downloaded**")
