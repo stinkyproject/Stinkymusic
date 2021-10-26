fb710b533f63bec08b030bc2829ece0f6784ae47
levina <82658782+levina-lab@users.noreply.github.com>
2021-10-26 23:27:52 +0700

    none

--- handlers/callback.py
+++ handlers/callback.py
@@ -22,7 +22,7 @@ async def cbstart(_, query: CallbackQuery):
 
 💡 **Find out all the Bot's commands and how they work by clicking on the » 📚 Commands button!**
 
-❔ **To know how to use this bot, please click on the » ❓ Basic Guide button!**""",
+🔖 **To know how to use this bot, please click on the » ❓ Basic Guide button!**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -191,8 +191,11 @@ async def cbguide(_, query: CallbackQuery):
 
 1.) **first, add me to your group.**
 2.) **then promote me as admin and give all permissions except anonymous admin.**
+3.) **after promoting me, type /reload in group to update the admin list.**
 3.) **add @{ASSISTANT_NAME} to your group or type /join to invite her.**
-4.) **turn on the voice chat first before start to play music.**
+4.) **turn on the video chat first before start to play music.**
+
+📌 **if the userbot not joined to video chat, make sure if the video chat already turned on, or type /leave then type /join again.**
 
 ⚡ __Powered by {BOT_NAME} A.I__""",
         reply_markup=InlineKeyboardMarkup(
