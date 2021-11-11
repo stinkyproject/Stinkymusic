86506d5d33a82a291276f4cfe8d09ce937a1b767
Hunter XDD <85411336+Hunter-XDD@users.noreply.github.com>
2021-11-11 13:27:39 +0530

    revert this change

--- handlers/play.py
+++ handlers/play.py
@@ -624,7 +624,7 @@ async def play(_, message: Message):
             url = f"https://youtube.com{results[0]['url_suffix']}"
             title = results[0]["title"][:70]
             thumbnail = results[0]["thumbnails"][0]
-            thumb_name = f"{title}.png"
+            thumb_name = f"{title}.jpg"
             ctitle = message.chat.title
             ctitle = await CHAT_TITLE(ctitle)
             thumb = requests.get(thumbnail, allow_redirects=True)
@@ -725,7 +725,7 @@ async def play(_, message: Message):
                 url = f"https://youtube.com{results[0]['url_suffix']}"
                 title = results[0]["title"][:70]
                 thumbnail = results[0]["thumbnails"][0]
-                thumb_name = f"{title}.png"
+                thumb_name = f"{title}.jpg"
                 ctitle = message.chat.title
                 ctitle = await CHAT_TITLE(ctitle)
                 thumb = requests.get(thumbnail, allow_redirects=True)
@@ -1015,7 +1015,7 @@ async def ytplay(_, message: Message):
         url = f"https://youtube.com{results[0]['url_suffix']}"
         title = results[0]["title"][:70]
         thumbnail = results[0]["thumbnails"][0]
-        thumb_name = f"{title}.png"
+        thumb_name = f"{title}.jpg"
         ctitle = message.chat.title
         ctitle = await CHAT_TITLE(ctitle)
         thumb = requests.get(thumbnail, allow_redirects=True)
