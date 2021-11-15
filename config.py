c85081af7ea22adf6344990d396759cabf9c8f97
Hunter XDD <85411336+Hunter-XDD@users.noreply.github.com>
2021-11-16 00:48:26 +0530

    umm

--- config.py
+++ config.py
@@ -3,9 +3,6 @@ from os import getenv
 from dotenv import load_dotenv
 from helpers.uptools import fetch_heroku_git_url
 
-if os.path.exists("local.env"):
-    load_dotenv("local.env")
-
 load_dotenv()
 que = {}
 admins = {}
